740350d46cc1c88458016808993ac70af38bcfd8
Yustaf Pramsistya <37360793+taftphir@users.noreply.github.com>
2025-09-20 17:02:44 +0700

    Updated mockData.ts

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -186,63 +186,65 @@ const generateHistoryData = (): HistoryRecord[] => {
         });
       }
     }
-    
-    let timestamp = new Date(now.getTime() - (0 * 2 * 60 * 60 * 1000)); // Every 2 hours
-    records.push({
-      id: `${vessel.id}-${0}`,
-      vesselId: vessel.id,
-      vesselName: vessel.name,
-      timestamp,
-      latitude: -7.38542,
-      longitude: 113.839851,
-      speed: Math.max(0, vessel.baseSpeed + (Math.random() - 0.5) * 8),
-      heading: Math.floor(Math.random() * 360),
-      rpmPortside: Math.max(0, vessel.baseRpmPortside + (Math.random() - 0.5) * 400),
-      rpmStarboard: Math.max(0, vessel.baseRpmStarboard + (Math.random() - 0.5) * 400),
-      rpmCenter: Math.max(0, vessel.baserpmCenter + (Math.random() - 0.5) * 400),
-    });
-    timestamp = new Date(now.getTime() - (1 * 2 * 60 * 60 * 1000)); // Every 2 hours
-    records.push({
-      id: `${vessel.id}-${1}`,
-      vesselId: vessel.id,
-      vesselName: vessel.name,
-      timestamp,
-      latitude: -7.384068,
-      longitude: 113.83886,
-      speed: Math.max(0, vessel.baseSpeed + (Math.random() - 0.5) * 8),
-      heading: Math.floor(Math.random() * 360),
-      rpmPortside: Math.max(0, vessel.baseRpmPortside + (Math.random() - 0.5) * 400),
-      rpmStarboard: Math.max(0, vessel.baseRpmStarboard + (Math.random() - 0.5) * 400),
-      rpmCenter: Math.max(0, vessel.baserpmCenter + (Math.random() - 0.5) * 400),
-    });
-    timestamp = new Date(now.getTime() - (2 * 2 * 60 * 60 * 1000)); // Every 2 hours
-    records.push({
-      id: `${vessel.id}-${2}`,
-      vesselId: vessel.id,
-      vesselName: vessel.name,
-      timestamp,
-      latitude: -7.383173,
-      longitude: 113.838173,
-      speed: Math.max(0, vessel.baseSpeed + (Math.random() - 0.5) * 8),
-      heading: Math.floor(Math.random() * 360),
-      rpmPortside: Math.max(0, vessel.baseRpmPortside + (Math.random() - 0.5) * 400),
-      rpmStarboard: Math.max(0, vessel.baseRpmStarboard + (Math.random() - 0.5) * 400),
-      rpmCenter: Math.max(0, vessel.baserpmCenter + (Math.random() - 0.5) * 400),
-    });
-    timestamp = new Date(now.getTime() - (3 * 2 * 60 * 60 * 1000)); // Every 2 hours
-    records.push({
-      id: `${vessel.id}-${3}`,
-      vesselId: vessel.id,
-      vesselName: vessel.name,
-      timestamp,
-      latitude: -7.382483,
-      longitude: 113.8377,
-      speed: Math.max(0, vessel.baseSpeed + (Math.random() - 0.5) * 8),
-      heading: Math.floor(Math.random() * 360),
-      rpmPortside: Math.max(0, vessel.baseRpmPortside + (Math.random() - 0.5) * 400),
-      rpmStarboard: Math.max(0, vessel.baseRpmStarboard + (Math.random() - 0.5) * 400),
-      rpmCenter: Math.max(0, vessel.baserpmCenter + (Math.random() - 0.5) * 400),
-    });
+
+    if (vessel.name=='Sinar Bahari') {
+      let timestamp = new Date(now.getTime() - (0 * 2 * 60 * 60 * 1000)); // Every 2 hours
+      records.push({
+        id: `${vessel.id}-${0}`,
+        vesselId: vessel.id,
+        vesselName: vessel.name,
+        timestamp,
+        latitude: -7.38542,
+        longitude: 113.839851,
+        speed: Math.max(0, vessel.baseSpeed + (Math.random() - 0.5) * 8),
+        heading: Math.floor(Math.random() * 360),
+        rpmPortside: Math.max(0, vessel.baseRpmPortside + (Math.random() - 0.5) * 400),
+        rpmStarboard: Math.max(0, vessel.baseRpmStarboard + (Math.random() - 0.5) * 400),
+        rpmCenter: Math.max(0, vessel.baserpmCenter + (Math.random() - 0.5) * 400),
+      });
+      timestamp = new Date(now.getTime() - (1 * 2 * 60 * 60 * 1000)); // Every 2 hours
+      records.push({
+        id: `${vessel.id}-${1}`,
+        vesselId: vessel.id,
+        vesselName: vessel.name,
+        timestamp,
+        latitude: -7.384068,
+        longitude: 113.83886,
+        speed: Math.max(0, vessel.baseSpeed + (Math.random() - 0.5) * 8),
+        heading: Math.floor(Math.random() * 360),
+        rpmPortside: Math.max(0, vessel.baseRpmPortside + (Math.random() - 0.5) * 400),
+        rpmStarboard: Math.max(0, vessel.baseRpmStarboard + (Math.random() - 0.5) * 400),
+        rpmCenter: Math.max(0, vessel.baserpmCenter + (Math.random() - 0.5) * 400),
+      });
+      timestamp = new Date(now.getTime() - (2 * 2 * 60 * 60 * 1000)); // Every 2 hours
+      records.push({
+        id: `${vessel.id}-${2}`,
+        vesselId: vessel.id,
+        vesselName: vessel.name,
+        timestamp,
+        latitude: -7.383173,
+        longitude: 113.838173,
+        speed: Math.max(0, vessel.baseSpeed + (Math.random() - 0.5) * 8),
+        heading: Math.floor(Math.random() * 360),
+        rpmPortside: Math.max(0, vessel.baseRpmPortside + (Math.random() - 0.5) * 400),
+        rpmStarboard: Math.max(0, vessel.baseRpmStarboard + (Math.random() - 0.5) * 400),
+        rpmCenter: Math.max(0, vessel.baserpmCenter + (Math.random() - 0.5) * 400),
+      });
+      timestamp = new Date(now.getTime() - (3 * 2 * 60 * 60 * 1000)); // Every 2 hours
+      records.push({
+        id: `${vessel.id}-${3}`,
+        vesselId: vessel.id,
+        vesselName: vessel.name,
+        timestamp,
+        latitude: -7.382483,
+        longitude: 113.8377,
+        speed: Math.max(0, vessel.baseSpeed + (Math.random() - 0.5) * 8),
+        heading: Math.floor(Math.random() * 360),
+        rpmPortside: Math.max(0, vessel.baseRpmPortside + (Math.random() - 0.5) * 400),
+        rpmStarboard: Math.max(0, vessel.baseRpmStarboard + (Math.random() - 0.5) * 400),
+        rpmCenter: Math.max(0, vessel.baserpmCenter + (Math.random() - 0.5) * 400),
+      });
+    }
   });
   
   return records.sort((a, b) => b.timestamp.getTime() - a.timestamp.getTime());
