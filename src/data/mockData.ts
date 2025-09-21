ecc4c5e861639824223be2c9ed675d0cef0b8f73
Yustaf Pramsistya <37360793+taftphir@users.noreply.github.com>
2025-09-22 02:11:45 +0700

    Updated mockData.ts

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -276,7 +276,7 @@ const generateHistoryData = (): HistoryRecord[] => {
         vesselId: vessel.id,
         vesselName: vessel.name,
         timestamp,
-        latitude: -7.242197,
+        latitude: -7.241197,
         longitude: 113.6377,
         speed: Math.max(0, vessel.baseSpeed + (Math.random() - 0.5) * 8),
         heading: Math.floor(Math.random() * 360),
